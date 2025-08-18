ba251fe50da3e19f15a7bb364d7ddc81b87dc44d
Butter <arranh2302@hotmail.com>
2025-08-18 19:07:31 +0100

    V4 source code
    Version 4 :)

--- CordToolSOURCE/CordTool/Program.cs
+++ CordToolSOURCE/CordTool/Program.cs
@@ -9,11 +9,20 @@ using DSharpPlus.CommandsNext;
 using System.Threading;
 using System.Security.Authentication;
 using System.Windows.Forms;
+using System.Runtime.CompilerServices;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Entities;
+using System.Collections.Concurrent;
 
 namespace CordTool
 {
     internal class Program
     {
+        static DiscordClient discord;
+        static DiscordGuild currentGuild;
+        static DiscordChannel currentChannel;
+        static ConcurrentQueue<string> messageQueue = new ConcurrentQueue<string>();
+
         static void Main(string[] args)
         {
             while (true)
@@ -62,6 +71,8 @@ namespace CordTool
                         break;
                     case '3':
                         loginBot();
+                        Thread.Sleep(10000);
+                        Console.Clear();
                         break;
                     case 'P':
                         releaseNotes();
@@ -108,7 +119,7 @@ namespace CordTool
         {
             Console.WriteLine("\n1. Send Webhook Message");
             Console.WriteLine("2. Nuke Server");
-            Console.WriteLine("3. Login to a Bot");
+            Console.WriteLine("3. Login to a Bot (IN DEV)");
             Console.WriteLine("4. Nuke Server Better (Reccommended)");
             Console.WriteLine("5. Exit");
             Console.WriteLine("P. Release Notes");
@@ -164,13 +175,123 @@ namespace CordTool
 
         }
 
-        static async void loginBot()
+        static async void loadinghaha()
         {
             Console.Clear();
-            Console.WriteLine("The log into a bot feature is in developement. Maybe try upgrade CordTool? ");
+            Console.WriteLine("Loading... 1%");
+            Thread.Sleep(5000);
+            Console.Clear();
+            Console.WriteLine("Loading... 32%");
+            Thread.Sleep(9000);
+            Console.Clear();
+            Console.WriteLine("Loading... 99%");
+            Thread.Sleep(900);
+            Console.Clear();
+            Console.WriteLine("Loading... 100%");
+            Console.Clear();
         }
 
-        static async void nukeServerBetter()
+
+        public static async Task loginBot()
+        {
+            Console.Clear();
+            Console.Write("Bot Token: ");
+            string token = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                Console.WriteLine("Token cannot be empty. Press any key to retry.");
+                Console.ReadKey();
+                await loginBot();
+                return;
+            }
+
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = token,
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.All
+            });
+
+            discord.Ready += (c, e) =>
+            {
+                Console.WriteLine($"Connected as {c.CurrentUser.Username}!");
+                return Task.CompletedTask;
+            };
+
+            discord.MessageCreated += (c, e) =>
+            {
+                if (currentChannel != null && e.Channel.Id == currentChannel.Id && !e.Author.IsBot)
+                    messageQueue.Enqueue($"[{e.Author.Username}] {e.Message.Content}");
+                return Task.CompletedTask;
+            };
+
+            await discord.ConnectAsync();
+
+            // Start CLI loop
+            while (true)
+            {
+                // Print queued messages
+                while (messageQueue.TryDequeue(out string msg))
+                    Console.WriteLine(msg);
+
+                Console.Write("> ");
+                string input = Console.ReadLine()?.Trim();
+                if (string.IsNullOrWhiteSpace(input)) continue;
+
+                switch (input.ToLower())
+                {
+                    case "help":
+                        Console.WriteLine("You can use the following commands: help, guilds, setguild, channels, setchannel, send, users.");
+                        break;
+                    case "guilds":
+                        foreach (var g in discord.Guilds.Values)
+                            Console.WriteLine($"{g.Name} ({g.Id})");
+                        break;
+                    case "setguild":
+                        Console.Write("Guild ID: ");
+                        if (ulong.TryParse(Console.ReadLine(), out ulong gid))
+                        {
+                            currentGuild = await discord.GetGuildAsync(gid);
+                            Console.WriteLine($"Selected guild: {currentGuild.Name}");
+                        }
+                        break;
+                    case "channels":
+                        if (currentGuild == null) { Console.WriteLine("Select guild first."); break; }
+                        foreach (var ch in currentGuild.Channels.Values)
+                            Console.WriteLine($"{ch.Name} ({ch.Id})");
+                        break;
+                    case "setchannel":
+                        Console.Write("Channel ID: ");
+                        if (ulong.TryParse(Console.ReadLine(), out ulong cid))
+                        {
+                            currentChannel = await discord.GetChannelAsync(cid);
+                            Console.WriteLine($"Selected channel: {currentChannel.Name}");
+                        }
+                        break;
+                    case "send":
+                        if (currentChannel == null) { Console.WriteLine("Select channel first."); break; }
+                        Console.Write("Message: ");
+                        string msgToSend = Console.ReadLine();
+                        await currentChannel.SendMessageAsync(msgToSend);
+                        break;
+                    case "users":
+                        if (currentGuild == null) { Console.WriteLine("Select guild first."); break; }
+                        foreach (var m in currentGuild.Members.Values)
+                            Console.WriteLine($"{m.Username}#{m.Discriminator}");
+                        break;
+                    case "exit":
+                        await discord.DisconnectAsync();
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command. Type 'help'.");
+                        Environment.Exit(1);
+                        break;
+                }
+            }
+        }
+
+            static async void nukeServerBetter()
         {
             Console.Clear();
             Console.Write("Bot Token: ");
@@ -186,10 +307,6 @@ namespace CordTool
             string serverInvite = Console.ReadLine();
             for (int i = 0; i < 1000000; i++)
             {
-
-
-
-
                 var discord = new DiscordClient(new DiscordConfiguration()
                 {
                     Token = Token,
@@ -267,6 +384,15 @@ namespace CordTool
                     Console.WriteLine("A GUI version has been added, along with a few bug fixes!");
                     Console.WriteLine("The GUI was made in Windows Forms, with simple C#, and the Visual Studio Designer view.");
                     Console.WriteLine("This version can be downloaded at: https://github.com/Epicinver/CordTool/releases/download/v3.0.0/Cordtool.zip");
+                    Console.WriteLine("Click any key to exit.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    break;
+                case '4':
+                    Console.WriteLine("Version 4.0.0 - 4th release, bot client is functional.");
+                    Console.WriteLine("The bot client is functional, but in development.");
+                    Console.WriteLine("I had fun making this version!");
+                    Console.WriteLine("This version can be downloaded at: https://github.com/Epicinver/CordTool/releases/download/v4.0.0/Cordtool.zip");
                     Console.WriteLine("Click any key to exit.");
                     Console.ReadKey();
                     Console.Clear();
